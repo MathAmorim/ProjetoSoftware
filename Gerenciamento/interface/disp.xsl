3d37e453a044358a7842718282f3eede023dd920
MathAmorim <matheus1998amorim@gmail.com>
2022-05-25 11:39:58 -0300

    MVC
    Model View Controler

---
+++ Gerenciamento/interface/disp.xsl
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<xsl:stylesheet version="1.0"
+xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+<xsl:template match="/">
+  <html>
+  <body>
+  <h2>Recurso</h2>
+  <table border="1">
+    <tr bgcolor="#9acd32">
+      <th>Data</th>
+      <th>Status</th>
+      <!--<th>Idade</th>-->
+    </tr>
+    <xsl:for-each select="recursos/recurso">
+    <tr>
+      <td><xsl:value-of select="data"/></td>
+      <td><a href="../controle/reserv.xml"><xsl:value-of select="status"/></a></td>
+      <!--<td><xsl:value-of select="idade"/></td>-->
+    </tr>
+    </xsl:for-each>
+  </table>
+  </body>
+  </html>
+</xsl:template>
+
+</xsl:stylesheet>
--- Gerenciamento/modelo/disp.php
+++ Gerenciamento/modelo/disp.php
@@ -3,38 +3,37 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>index</title>
+  	<meta http-equiv='refresh' content='0; URL=../controle/disp.xml'>
+	<title>Disponibilidade</title>
 </head>
 <body>
 	<?php
 	$res = array();
-	$tp = array('Projetor','Laboratorio');
+	$tp = array('Disponivel','Reservado');
 	for ($i=0; $i < 10; $i++) { 
-		$tipo = $tp[rand(0, count($tp)-1)];
-	   	$res[$i]['id'] = $i;
-	    $res[$i]['tipo'] = $tipo;
-	    //$res[$i]['identificacao'] = $identificacao;
+		$status = $tp[rand(0, count($tp)-1)];
+	   	$res[$i]['data'] = $i+1;
+	    $res[$i]['status'] = $status;
+	    //$res[$i]['reserva'] = $reserva;
 	}
 
 	$xml = '<?xml version="1.0"?>';
-	$xml = '<?xml-stylesheet type="text/xsl" href="../interface/recursos.xsl"?>';
+	$xml = '<?xml-stylesheet type="text/xsl" href="../interface/disp.xsl"?>';
 	$xml .= '<recursos>';
 
 	for ( $i = 0; $i < count( $res ); $i++ ) {
 		$xml .= '<recurso>';
-		$xml .= '<id>' . $res[$i]['id'] . '</id>';
-		$xml .= '<tipo>' . $res[$i]['tipo'] . '</tipo>';
+		$xml .= '<data>' . $res[$i]['data'] .'/6/2022'. '</data>';
+		$xml .= '<status>' . $res[$i]['status'] . '</status>';
 		//$xml .= '<identificacao>' . $res[$i]['identificacao'] . '</identificacao>';
 		$xml .= '</recurso>';
 	}
 
 	$xml .= '</recursos>';
 
-	$fp = fopen('controle/recursos.xml', 'w+');
+	$fp = fopen('../controle/disp.xml', 'w+');
 	fwrite($fp, $xml);
 	fclose($fp);
-	echo "XML gerado! <br>";
-	echo '<a href="controle/recursos.xml">Abrir XML</a>';
 	?>
 </body>
 </html>
